c317cff6b3981791b194090cb02c082b00a89770
lcarranco <lcarranco@gmail.com>
2017-01-21 17:36:02 -0600

    Testing isalnum function

--- count.cpp
+++ count.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
 int main (int argc, char * argv[2])
 {
-    cout << "Hello World" << endl;
+    int i;
+    char str[]="c3po...";
+    i=0;
+    while (isalnum(str[i])) i++;
+    printf ("The first %d characters are alphanumeric.\n",i);
+    return 0;
 }

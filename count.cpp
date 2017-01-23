0ed96b764af776c01e32a52899fec532d918f0a8
lcarranco <lcarranco@gmail.com>
2017-01-23 00:24:10 -0600

    Removed using namespace std from count.cpp file

--- count.cpp
+++ count.cpp
@@ -5,21 +5,19 @@
 #include <fstream>
 #include "readFile.h"
 
-using namespace std;
-
 int main (int argc, char * argv[2])
 {
     ReadFile a;
     a.readFile();
 
     //Initialized vector
-    vector<int> myVector;
+    std::vector<int> myVector;
 
 
     int i;
     char str[]="c.3po...";
     i=0;
-    while (isalnum(str[i])) i++;
-    printf ("The first %d characters are alphanumeric.\n",i);
+    while (std::isalnum(str[i])) i++;
+    std::printf ("The first %d characters are alphanumeric.\n",i);
     return 0;
 }

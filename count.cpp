ac9c4415bc1e8b96c86e6c876c3debe68a6bfd04
lcarranco <lcarranco@gmail.com>
2017-01-23 14:36:56 -0600

    Added definition for is_number function

--- count.cpp
+++ count.cpp
@@ -32,3 +32,10 @@ int main(int argc, char* argv[])
 		
   return 0;
 }
+
+bool is_number(const std::string& s)
+{
+  std::string::const_iterator it = s.begin();
+  while (it != s.end() && std::isdigit(*it)) ++it;
+  return !s.empty() && it == s.end();
+}

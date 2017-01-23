8061eb1d839d5607f33fe9177418efe3734b9561
lcarranco <lcarranco@gmail.com>
2017-01-22 23:26:41 -0600

    Added an include guard

--- readFile.h
+++ readFile.h
@@ -1,5 +1,11 @@
+#ifdef _readFile_h_INCLUDED //if readFile.h has not been included yet...
+#define _readFile_h_INCLUDED //#define this so the comiler knows it has been included
+
+
 class ReadFile
 {
     public:
         void readFile();
 };
+
+#endif // !_readFile_h_INCLUDED

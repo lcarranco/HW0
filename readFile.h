70b5be44f0db0c56154c07e0cafe976045f82445
lcarranco <lcarranco@gmail.com>
2017-01-22 23:49:50 -0600

    Fixed include guard

--- readFile.h
+++ readFile.h
@@ -1,5 +1,5 @@
 //include guard
-#ifdef _readFile_h_INCLUDED //if readFile.h has not been included yet...
+#ifndef _readFile_h_INCLUDED //if readFile.h has not been included yet...
 #define _readFile_h_INCLUDED //#define this so the comiler knows it has been included
 
 

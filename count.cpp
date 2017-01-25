387ec192b2aa3aa60aad789379cb9f85e9702030
lcarranco <lcarranco@gmail.com>
2017-01-25 15:14:16 -0600

    Almost got this working.
    Need to figure out how to iterate through a vector and print the contenst.

--- count.cpp
+++ count.cpp
@@ -8,6 +8,21 @@
 
 using namespace std;
 
+//Copied from https://www.tutorialspoint.com/cplusplus/cpp_templates.htm
+template <class T>
+class Stack { 
+   private: 
+      vector<T> elems;     // elements 
+
+   public: 
+      void push(T const&);  // push element 
+      void pop();               // pop element 
+      T top() const;            // return top element 
+      bool empty() const{       // return true if empty.
+         return elems.empty(); 
+      } 
+};
+
 bool is_number(const std::string& s);
 
 int main(int argc, char* argv[])
@@ -17,23 +32,40 @@ int main(int argc, char* argv[])
   }
   ArgumentManager am(argc, argv);
   std::string filename = am.get("filename");
-  cout << filename << endl;
   std::ifstream ifs(filename.c_str());
   std::string line;
+  int wordCount = 0, numberCount;
+  vector<string> wordVector;
   while (getline(ifs, line)){
-    // replace symbols by space for line.
-    // ...
+    //replace symbols by space for line.
+    //...
+    //Got the following code from http://stackoverflow.com/questions/7621727/split-a-string-into-words-by-multiple-delimiters
+    std::size_t prev = 0, pos;
+    while ((pos = line.find_first_of(" ';\n\\~`!@#$%^&*()-_+={}[]|:<,>.?/", prev)) != std::string::npos)
+    {
+        if (pos > prev)
+            wordVector.push_back(line.substr(prev, pos-prev));
+        prev = pos+1;
+    }
+    if (prev < line.length()) {
+        wordVector.push_back(line.substr(prev, std::string::npos));
+    }
+    //End of StackOverflow code
+    for(int i = 0; i < wordVector.size(); i++) {
+        cout << i << endl;
+    }
     std::stringstream ss(line.c_str());
     std::string str;
     while (ss >> str) {
       if (is_number(str)) {
-	// ...
+        numberCount++;
       }
       else {
-      // ...
+        wordCount++;
       }
     }
   }
+  cout << numberCount << endl;
 		
   return 0;
 }
